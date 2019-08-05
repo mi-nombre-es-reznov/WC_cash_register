593f157e4021f0563863e4b3d764ef4d76663748
Nicholas Perez-Aguilar <npere017@ucr.edu>
2019-08-05 05:45:22 -0700

    Picture has been added along with basic calculator functionalities.

--- WC_cash_register/Cash_register_UI.h
+++ WC_cash_register/Cash_register_UI.h
@@ -57,6 +57,13 @@ namespace WCcashregister {
 	private: System::Windows::Forms::Button^  button15;
 	private: System::Windows::Forms::Button^  button16;
 	private: System::Windows::Forms::PictureBox^  pictureBox1;
+	private: System::Windows::Forms::GroupBox^  groupBox3;
+	private: System::Windows::Forms::Button^  button21;
+	private: System::Windows::Forms::Button^  button20;
+	private: System::Windows::Forms::Button^  button19;
+	private: System::Windows::Forms::Button^  button18;
+	private: System::Windows::Forms::Button^  button22;
+	private: System::Windows::Forms::Label^  label2;
 
 
 
@@ -97,9 +104,17 @@ namespace WCcashregister {
 			this->button15 = (gcnew System::Windows::Forms::Button());
 			this->button16 = (gcnew System::Windows::Forms::Button());
 			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->groupBox3 = (gcnew System::Windows::Forms::GroupBox());
+			this->button22 = (gcnew System::Windows::Forms::Button());
+			this->button21 = (gcnew System::Windows::Forms::Button());
+			this->button20 = (gcnew System::Windows::Forms::Button());
+			this->button19 = (gcnew System::Windows::Forms::Button());
+			this->button18 = (gcnew System::Windows::Forms::Button());
+			this->label2 = (gcnew System::Windows::Forms::Label());
 			this->groupBox1->SuspendLayout();
 			this->groupBox2->SuspendLayout();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			this->groupBox3->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// button1
@@ -112,6 +127,7 @@ namespace WCcashregister {
 			this->button1->TabIndex = 0;
 			this->button1->Text = L"7";
 			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &Cash_register_UI::Buttons_clicked);
 			// 
 			// button2
 			// 
@@ -123,6 +139,7 @@ namespace WCcashregister {
 			this->button2->TabIndex = 1;
 			this->button2->Text = L"8";
 			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &Cash_register_UI::Buttons_clicked);
 			// 
 			// button3
 			// 
@@ -134,6 +151,7 @@ namespace WCcashregister {
 			this->button3->TabIndex = 2;
 			this->button3->Text = L"9";
 			this->button3->UseVisualStyleBackColor = true;
+			this->button3->Click += gcnew System::EventHandler(this, &Cash_register_UI::Buttons_clicked);
 			// 
 			// button4
 			// 
@@ -145,6 +163,7 @@ namespace WCcashregister {
 			this->button4->TabIndex = 5;
 			this->button4->Text = L"6";
 			this->button4->UseVisualStyleBackColor = true;
+			this->button4->Click += gcnew System::EventHandler(this, &Cash_register_UI::Buttons_clicked);
 			// 
 			// button5
 			// 
@@ -156,6 +175,7 @@ namespace WCcashregister {
 			this->button5->TabIndex = 4;
 			this->button5->Text = L"5";
 			this->button5->UseVisualStyleBackColor = true;
+			this->button5->Click += gcnew System::EventHandler(this, &Cash_register_UI::Buttons_clicked);
 			// 
 			// button6
 			// 
@@ -167,6 +187,7 @@ namespace WCcashregister {
 			this->button6->TabIndex = 3;
 			this->button6->Text = L"4";
 			this->button6->UseVisualStyleBackColor = true;
+			this->button6->Click += gcnew System::EventHandler(this, &Cash_register_UI::Buttons_clicked);
 			// 
 			// button7
 			// 
@@ -178,6 +199,7 @@ namespace WCcashregister {
 			this->button7->TabIndex = 8;
 			this->button7->Text = L"3";
 			this->button7->UseVisualStyleBackColor = true;
+			this->button7->Click += gcnew System::EventHandler(this, &Cash_register_UI::Buttons_clicked);
 			// 
 			// button8
 			// 
@@ -189,6 +211,7 @@ namespace WCcashregister {
 			this->button8->TabIndex = 7;
 			this->button8->Text = L"2";
 			this->button8->UseVisualStyleBackColor = true;
+			this->button8->Click += gcnew System::EventHandler(this, &Cash_register_UI::Buttons_clicked);
 			// 
 			// button9
 			// 
@@ -200,6 +223,7 @@ namespace WCcashregister {
 			this->button9->TabIndex = 6;
 			this->button9->Text = L"1";
 			this->button9->UseVisualStyleBackColor = true;
+			this->button9->Click += gcnew System::EventHandler(this, &Cash_register_UI::Buttons_clicked);
 			// 
 			// button10
 			// 
@@ -211,6 +235,7 @@ namespace WCcashregister {
 			this->button10->TabIndex = 11;
 			this->button10->Text = L"Clear";
 			this->button10->UseVisualStyleBackColor = true;
+			this->button10->Click += gcnew System::EventHandler(this, &Cash_register_UI::button10_Click);
 			// 
 			// button11
 			// 
@@ -222,6 +247,7 @@ namespace WCcashregister {
 			this->button11->TabIndex = 10;
 			this->button11->Text = L"0";
 			this->button11->UseVisualStyleBackColor = true;
+			this->button11->Click += gcnew System::EventHandler(this, &Cash_register_UI::Buttons_clicked);
 			// 
 			// button12
 			// 
@@ -233,15 +259,16 @@ namespace WCcashregister {
 			this->button12->TabIndex = 9;
 			this->button12->Text = L".";
 			this->button12->UseVisualStyleBackColor = true;
+			this->button12->Click += gcnew System::EventHandler(this, &Cash_register_UI::button12_Click);
 			// 
 			// textBox1
 			// 
 			this->textBox1->BackColor = System::Drawing::SystemColors::Window;
-			this->textBox1->Font = (gcnew System::Drawing::Font(L"Goudy Stout", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+			this->textBox1->Font = (gcnew System::Drawing::Font(L"Gadugi", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->textBox1->Location = System::Drawing::Point(63, 31);
 			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(396, 44);
+			this->textBox1->Size = System::Drawing::Size(396, 43);
 			this->textBox1->TabIndex = 12;
 			this->textBox1->Text = L"0";
 			this->textBox1->TextAlign = System::Windows::Forms::HorizontalAlignment::Right;
@@ -273,7 +300,7 @@ namespace WCcashregister {
 			this->label1->AutoSize = true;
 			this->label1->Font = (gcnew System::Drawing::Font(L"Monotype Corsiva", 50.25F, static_cast<System::Drawing::FontStyle>((System::Drawing::FontStyle::Bold | System::Drawing::FontStyle::Italic)),
 				System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(0)));
-			this->label1->Location = System::Drawing::Point(653, 9);
+			this->label1->Location = System::Drawing::Point(667, 9);
 			this->label1->Name = L"label1";
 			this->label1->Size = System::Drawing::Size(568, 82);
 			this->label1->TabIndex = 15;
@@ -288,7 +315,7 @@ namespace WCcashregister {
 			this->groupBox1->Size = System::Drawing::Size(396, 80);
 			this->groupBox1->TabIndex = 16;
 			this->groupBox1->TabStop = false;
-			this->groupBox1->Text = L"Common";
+			this->groupBox1->Text = L"Common Transaction Tools";
 			// 
 			// groupBox2
 			// 
@@ -345,11 +372,95 @@ namespace WCcashregister {
 			this->pictureBox1->TabIndex = 18;
 			this->pictureBox1->TabStop = false;
 			// 
+			// groupBox3
+			// 
+			this->groupBox3->Controls->Add(this->button22);
+			this->groupBox3->Controls->Add(this->button21);
+			this->groupBox3->Controls->Add(this->button20);
+			this->groupBox3->Controls->Add(this->button19);
+			this->groupBox3->Controls->Add(this->button18);
+			this->groupBox3->Location = System::Drawing::Point(494, 337);
+			this->groupBox3->Name = L"groupBox3";
+			this->groupBox3->Size = System::Drawing::Size(127, 299);
+			this->groupBox3->TabIndex = 19;
+			this->groupBox3->TabStop = false;
+			this->groupBox3->Text = L"Common Operations";
+			// 
+			// button22
+			// 
+			this->button22->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button22->Location = System::Drawing::Point(6, 242);
+			this->button22->Name = L"button22";
+			this->button22->Size = System::Drawing::Size(114, 49);
+			this->button22->TabIndex = 20;
+			this->button22->Text = L"=";
+			this->button22->UseVisualStyleBackColor = true;
+			this->button22->Click += gcnew System::EventHandler(this, &Cash_register_UI::button22_Click);
+			// 
+			// button21
+			// 
+			this->button21->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button21->Location = System::Drawing::Point(6, 187);
+			this->button21->Name = L"button21";
+			this->button21->Size = System::Drawing::Size(114, 49);
+			this->button21->TabIndex = 19;
+			this->button21->Text = L"/";
+			this->button21->UseVisualStyleBackColor = true;
+			this->button21->Click += gcnew System::EventHandler(this, &Cash_register_UI::Operations);
+			// 
+			// button20
+			// 
+			this->button20->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button20->Location = System::Drawing::Point(6, 132);
+			this->button20->Name = L"button20";
+			this->button20->Size = System::Drawing::Size(114, 49);
+			this->button20->TabIndex = 18;
+			this->button20->Text = L"*";
+			this->button20->UseVisualStyleBackColor = true;
+			this->button20->Click += gcnew System::EventHandler(this, &Cash_register_UI::Operations);
+			// 
+			// button19
+			// 
+			this->button19->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button19->Location = System::Drawing::Point(6, 77);
+			this->button19->Name = L"button19";
+			this->button19->Size = System::Drawing::Size(114, 49);
+			this->button19->TabIndex = 17;
+			this->button19->Text = L"-";
+			this->button19->UseVisualStyleBackColor = true;
+			this->button19->Click += gcnew System::EventHandler(this, &Cash_register_UI::Operations);
+			// 
+			// button18
+			// 
+			this->button18->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button18->Location = System::Drawing::Point(6, 22);
+			this->button18->Name = L"button18";
+			this->button18->Size = System::Drawing::Size(114, 49);
+			this->button18->TabIndex = 16;
+			this->button18->Text = L"+";
+			this->button18->UseVisualStyleBackColor = true;
+			this->button18->Click += gcnew System::EventHandler(this, &Cash_register_UI::Operations);
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(69, 37);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(0, 13);
+			this->label2->TabIndex = 20;
+			// 
 			// Cash_register_UI
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(1270, 643);
+			this->ClientSize = System::Drawing::Size(1270, 663);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->groupBox3);
 			this->Controls->Add(this->pictureBox1);
 			this->Controls->Add(this->groupBox2);
 			this->Controls->Add(this->groupBox1);
@@ -369,13 +480,83 @@ namespace WCcashregister {
 			this->Controls->Add(this->button1);
 			this->Name = L"Cash_register_UI";
 			this->Text = L"Cash_register_UI";
+			this->Click += gcnew System::EventHandler(this, &Cash_register_UI::Operations);
 			this->groupBox1->ResumeLayout(false);
 			this->groupBox2->ResumeLayout(false);
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			this->groupBox3->ResumeLayout(false);
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
 		}
+
 #pragma endregion
+// Global Variables
+		double first_num;
+		double second_num;
+		double result;
+		String^ operators;
+
+private: System::Void Buttons_clicked(System::Object^  sender, System::EventArgs^  e) {
+	Button ^ Numbers = safe_cast<Button^>(sender);
+
+	if (textBox1->Text == "0")
+	{
+		textBox1->Text = Numbers->Text;
+	}
+	else
+	{
+		textBox1->Text = textBox1->Text + Numbers->Text;
+	}
+}
+
+private: System::Void Operations(System::Object^  sender, System::EventArgs^  e) {
+
+	Button ^ Ops = safe_cast<Button^>(sender);
+
+	first_num = Double::Parse(textBox1->Text);
+	textBox1->Text = "";
+	operators = Ops->Text;
+	label2->Text = System::Convert::ToString(first_num) + " " + operators;
+}
+
+private: System::Void button10_Click(System::Object^  sender, System::EventArgs^  e) {
+	textBox1->Text = "0";
+	label2->Text = "";
+}
+
+private: System::Void button22_Click(System::Object^  sender, System::EventArgs^  e) {
+	label2->Text = "";
+	second_num = Double::Parse(textBox1->Text);
+
+	if (operators == "+")
+	{
+		result = first_num + second_num;
+		textBox1->Text = System::Convert::ToString(result);
+	}
+	else if (operators == "-")
+	{
+		result = first_num - second_num;
+		textBox1->Text = System::Convert::ToString(result);
+	}
+	else if (operators == "*")
+	{
+		result = first_num * second_num;
+		textBox1->Text = System::Convert::ToString(result);
+	}
+	else if (operators == "/")
+	{
+		result = first_num / second_num;
+		textBox1->Text = System::Convert::ToString(result);
+	}
+}
+
+private: System::Void button12_Click(System::Object^  sender, System::EventArgs^  e) {
+	if (!textBox1->Text->Contains("."))
+	{
+		textBox1->Text = textBox1->Text + ".";
+	}
+}
+
 };
 }
