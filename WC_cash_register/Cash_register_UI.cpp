9d6fa37e9e90e0c681ec57cce759897d24fc612f
Nicholas Perez-Aguilar <npere017@ucr.edu>
2019-08-05 04:01:02 -0700

    Initial UI design. No connections amongst buttons/labels.

---
+++ WC_cash_register/Cash_register_UI.cpp
@@ -0,0 +1,15 @@
+#include "Cash_register_UI.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+
+[STAThread]
+void Main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+
+	WCcashregister::Cash_register_UI form;
+	Application::Run(%form);
+}
